fcf3ab9347e137f9451ff61a0b6a447297d13496
Patrick Coyle <36138086+patrickcoyle427@users.noreply.github.com>
2018-05-03 15:15:55 -0400

    Add files via upload

--- createNewTournament.py
+++ createNewTournament.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
-# TODO:
-# Add section to set rounds, or let the software calculate rounds
-# Error handling?
-# Make it so you can create events with the same name (Maybe create an event number?)
+'''
+createNewTournament.py - creates a new tournament, based on user's choices
+                         in the GUI. Writes these choices to an XML file
+                         that will be used for the rest of the event.
+'''
 
-import sys, os
+import sys, os, random
 
 import xml.etree.ElementTree as ET
 
 from PyQt5.QtWidgets import (QWidget, QPushButton, QApplication,
                              QCheckBox, QRadioButton, QLabel,
                              QGridLayout, QLineEdit, QHBoxLayout,
-                             QGroupBox, QVBoxLayout)
+                             QGroupBox, QVBoxLayout, QMessageBox)
 
 from PyQt5.QtCore import Qt
 
@@ -28,14 +29,12 @@ class CreateTournament(QWidget):
 
         window_layout = QVBoxLayout()
         self.setLayout(window_layout)
-
-        #layout = QGridLayout()
-        #self.setLayout(layout)
-        # Sets up a grid layout for the window.
+        # Sets a vertical layout for the window itself
 
         ### Event Name Section ###
 
         event_name_layout = QHBoxLayout()
+        # Holds the event name layout
 
         self.event_name_label = QLabel('Event Name:', self)
         # Creates the label for event name
@@ -45,58 +44,80 @@ class CreateTournament(QWidget):
 
         event_name_layout.addWidget(self.event_name_label)
         event_name_layout.addWidget(self.enter_event_name)
+        # Adds the event name to the layout
 
         window_layout.addLayout(event_name_layout)
+        # Adds the event_name_layout to the window
 
         ### Tournament Type Section ###
 
-        type_and_games_layout = QHBoxLayout()
+        option_layout = QHBoxLayout()
+        # Layout Container for the tournament option selections
 
         self.tournamentTypeGroup = QGroupBox('Tourament Type:')
         # Creates a group of radio button. This one is for the tournament type
         
         self.tournament_type = (QRadioButton('Swiss'), QRadioButton('Single Elimination'))
         # Tuple of the different types of tournaments
+
+        # For reference:
+        # self.tournament_type[0] is Swiss Style (no one is auto eliminated)
+        # self.tournament_type[1] is single elimination (dropped after loss)
+        
         self.tournament_type[0].setChecked(True)
         # Sets 'Swiss' to be checked by default
 
         tournament_type_layout = QVBoxLayout()
-        tournament_type_layout.addWidget(self.tournament_type[0])
-        tournament_type_layout.addWidget(self.tournament_type[1])
+
+        for tType in self.tournament_type:
+            
+            tournament_type_layout.addWidget(tType)
 
         self.tournamentTypeGroup.setLayout(tournament_type_layout)
 
-        type_and_games_layout.addWidget(self.tournamentTypeGroup)
+        option_layout.addWidget(self.tournamentTypeGroup)
 
         ### Games Played Section ###
 
         self.gamesPlayedGroup = QGroupBox('Games In Match:')
 
         self.games_played = (QRadioButton('Best of 1'), QRadioButton('Best of 3'))
-
         # In a tuple together for organization purposes
 
+        # For reference:
+        # self.games_played[0] is Best of 1 (only 1 game in match is played)
+        # self.games_played[1] is Best of 3 (best 2 of 3 games for match
+
         games_played_layout = QVBoxLayout()
-        games_played_layout.addWidget(self.games_played[0])
-        games_played_layout.addWidget(self.games_played[1])
+
+        for gPlayed in self.games_played:
+            
+            games_played_layout.addWidget(gPlayed)
+
+        # Loop to add buttons to the layout. Done this way to save time later
+        # if another option needs to be added to games_played
 
         self.games_played[0].setChecked(True)
+        # By default, best of 1 is selected
 
         self.gamesPlayedGroup.setLayout(games_played_layout)
 
-        type_and_games_layout.addWidget(self.gamesPlayedGroup)
-
-        #window_layout.addLayout(type_and_games_layout)
+        option_layout.addWidget(self.gamesPlayedGroup)
+        # Adds games played
 
         ### Create Draft Pods Section ###
 
         self.draftPodGroup = QGroupBox('Draft Pods:')
+        # Creates a container for the draftPodOPtions
 
         self.create_draft_pods = QCheckBox('Create Draft Pods')
         self.create_draft_pods.stateChanged.connect(self.draft_settings)
+        # draft_settings enables the play_in_pods option below.
         
         self.play_in_pods = QCheckBox('Play Within Pods')
         self.play_in_pods.setEnabled(False)
+        # Play within pods disabled until the create draft pods option
+        # is selected.
 
         draft_pod_layout = QVBoxLayout()
         draft_pod_layout.addWidget(self.create_draft_pods)
@@ -104,18 +125,19 @@ class CreateTournament(QWidget):
 
         self.draftPodGroup.setLayout(draft_pod_layout)
 
-        type_and_games_layout.addWidget(self.draftPodGroup)
+        option_layout.addWidget(self.draftPodGroup)
 
-        window_layout.addLayout(type_and_games_layout)
+        window_layout.addLayout(option_layout)
 
         ### Create Tournament and Cancel Section ###
 
         buttons = QHBoxLayout()
 
+        # Creates a layout for the buttons
+
         self.create_event = QPushButton('Create Event', self)
         self.cancel = QPushButton('Cancel', self)
 
-        #self.create_event.clicked.connect(self.test)
         self.create_event.clicked.connect(self.write_tournament_xml_file)
         self.cancel.clicked.connect(self.close)
 
@@ -126,10 +148,11 @@ class CreateTournament(QWidget):
         buttons.addWidget(self.cancel)
 
         window_layout.addLayout(buttons)
-        
 
+        ### Window Options ###
+        
         self.setWindowTitle('Create New Tournament')
-        self.setGeometry(300, 300, 150, 150)
+        self.setFixedSize(400, 175)
         self.setWindowFlags(Qt.WindowCloseButtonHint)
         # Sets it so the user can only close the window, not minimize or maximize
 
@@ -158,85 +181,131 @@ class CreateTournament(QWidget):
 
     def write_tournament_xml_file(self):
 
-        tournament_file = ET.Element('Tournament')
+        def write_file():
 
-        # Sets up the tournament's XML file. The program will read this
-        # to set up the event, and will write what happens in the event to
-        # this file.
+            tournament_file = ET.Element('Tournament')
 
-        tournamentSetup = ET.SubElement(tournament_file, 'TournamentSetup')
+            # Sets up the tournament's XML file. The program will read this
+            # to set up the event, and will write what happens in the event to
+            # this file.
 
-        eventName = ET.SubElement(tournamentSetup, 'EventName')
-        eventName.text = self.enter_event_name.text()
+            tournamentSetup = ET.SubElement(tournament_file, 'TournamentSetup')
 
-        structure = ET.SubElement(tournamentSetup, 'Structure')
+            eventName = ET.SubElement(tournamentSetup, 'EventName')
+            eventName.text = self.enter_event_name.text()
 
-        style = ET.SubElement(structure, 'Style')
+            structure = ET.SubElement(tournamentSetup, 'Structure')
 
-        if self.tournament_type[0].isChecked() == True:
+            style = ET.SubElement(structure, 'Style')
 
-            style.text = 'Swiss'
+            if self.tournament_type[0].isChecked() == True:
+
+                style.text = 'Swiss'
+
+            else:
+
+                style.text = 'SingleElimination'
+
+            gamesPlayedInMatch = ET.SubElement(structure, 'GamesPlayedInMatch')
+
+            if self.games_played[0].isChecked() == True:
+                
+                gamesPlayedInMatch.text = '1'
+
+            else:
+
+                gamesPlayedInMatch.text = '3'
+
+            draftpods = ET.SubElement(structure, 'DraftPods')
+
+            if self.create_draft_pods.isChecked() == True:
+
+                draftpods.text = 'True'
+
+            else:
+
+                draftpods.text = 'False'
+
+            playInPods = ET.SubElement(structure, 'PlayInPods')
+
+            if self.play_in_pods.isChecked() == True:
+
+                playInPods.text = 'True'
+
+            else:
+
+                playInPods.text = 'False'
+
+            players = ET.SubElement(tournament_file, 'Players')
+            rounds = ET.SubElement(tournament_file, 'Rounds')
+
+            # The create tournament screen pre-writes the players and rounds
+            # subelements. These will be used during player enrollment and
+            # the creation of rounds respectively
+
+            tournament_directory = 'Tournaments'
+
+            if not os.path.isdir(tournament_directory):
+
+                os.mkdir(tournament_directory)
+                
+            # Creates the Tournaments directory if it does not exist.
+            # Tournaments will all be written to this folder.
+
+            idNumber = str(random.randint(1000000, 9999999))
+            # Creates a large random ID number to help prevent the user
+            # from accidently creating an event with the same name as an
+            # existing one and writing over the old one.
+
+            path = os.path.join(tournament_directory,
+                                '{} ID{}.xml'.format(self.enter_event_name.text(),
+                                                       idNumber))
+
+            # Builds the path and file name to the tournament file
+
+            while os.path.exists(path):
+
+                idNumber = str(random.randint(1000000, 9999999))
+                # If a tournament with the same name AND ID number exist,
+                # the ID number is rerolled until it finds one not in use.
+
+                path = os.path.join(tournament_directory,
+                                    '{} (ID{}).xml'.format(self.enter_event_name.text(),
+                                                           idNumber))
+
+            setIdNumber = ET.SubElement(tournamentSetup, 'IDNumber')
+            setIdNumber.text = idNumber
+            # Saves the ID number to the file.
+
+            to_write = ET.ElementTree(tournament_file)
+
+            to_write.write('{}'.format(path),
+                                 encoding = 'utf-8',
+                                 xml_declaration = True)
+            # Writes the tournament file
+
+            self.close()
+            # Closes the event creation screen once it is done.
+
+        if self.enter_event_name.text().strip() == '':
+
+            # Check to make sure tournament name isn't blank or a bunch
+            # of spaces. Displays an error message and prevents the event from being written
+
+            error = QMessageBox()
+            error.setIcon(QMessageBox.Information)
+            error.setText('No Tournament Name Set')
+            error.setWindowTitle('Error')
+
+            self.enter_event_name.setText('')
+
+            self.enter_event_name.setFocus()
+
+            error.exec_()
 
         else:
 
-            style.text = 'SingleElimination'
-
-        gamesPlayedInMatch = ET.SubElement(structure, 'GamesPlayedInMatch')
-
-        if self.games_played[0].isChecked() == True:
-            
-            gamesPlayedInMatch.text = '1'
-
-        else:
-
-            gamesPlayedInMatch.text = '3'
-
-        draftpods = ET.SubElement(structure, 'DraftPods')
-
-        if self.create_draft_pods.isChecked() == True:
-
-            draftpods.text = 'True'
-
-        else:
-
-            draftpods.text = 'False'
-
-        playInPods = ET.SubElement(structure, 'PlayInPods')
-
-        if self.play_in_pods.isChecked() == True:
-
-            playInPods.text = 'True'
-
-        else:
-
-            playInPods.text = 'False'
-
-        players = ET.SubElement(tournament_file, 'Players')
-        rounds = ET.SubElement(tournament_file, 'Rounds')
-
-        # The create tournament screen pre-writes the players and rounds
-        # subelements. These will be used during player enrollment and
-        # the creation of rounds respectively
-
-        tournament_directory = 'Tournaments'
-
-        if not os.path.isdir(tournament_directory):
-
-            os.mkdir(tournament_directory)
-
-        # Creates the Tournaments directory if it does not exist.
-        # Tournaments will all be written to this folder.
-
-        to_write = ET.ElementTree(tournament_file)
-
-        path = os.path.join(tournament_directory, '{}.xml'.format(self.enter_event_name.text()))
-
-        to_write.write('{}'.format(path),
-                             encoding = 'utf-8',
-                             xml_declaration = True)
-        # Writes the xml file
-
-        self.close()
+            write_file()
 
 if __name__ == '__main__':
 
