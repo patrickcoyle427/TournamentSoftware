76cdb7a60052bba903365d87baebd8cb145597e3
Patrick Coyle <36138086+patrickcoyle427@users.noreply.github.com>
2018-05-08 10:53:12 -0400

    Add files via upload

--- enrollPlayers.py
+++ enrollPlayers.py
@@ -9,7 +9,7 @@ enrollPlayers.py - enroll players into an event and write them to
 # TODO:
 
 # Implelment how the window will look. I think the enrollment section should
-#     be on the left and the tableview of all the players should be right
+#   be on the left and the tableview of all the players should be right
 #
 # Have this load the XML tournament file and write the players to it
 #
@@ -18,6 +18,10 @@ enrollPlayers.py - enroll players into an event and write them to
 # Add way to add own ID numbers instead of just using temp IDs
 #
 # Make addPlayer functional
+#
+# Make the cells of the table able to be right clicked with custom
+#   menu. You should be able to change the player's name or remove them
+#   from the list
 
 import sys, os, random
 
@@ -25,7 +29,8 @@ import xml.etree.ElementTree as ET
 
 from PyQt5.QtWidgets import (QWidget, QPushButton, QApplication,
                              QTableWidgetItem, QAbstractItemView,
-                             QLabel, QLineEdit, QVBoxLayout, QHBoxLayout)
+                             QLabel, QLineEdit, QVBoxLayout,
+                             QHBoxLayout, QTableWidget, QHeaderView)
 
 from PyQt5.QtCore import Qt
 
@@ -46,11 +51,13 @@ class EnrollPlayers(QWidget):
 
         window_layout = QHBoxLayout()
         self.setLayout(window_layout)
+        window_layout.setSpacing(5)
         # Sets a horizontal layout for the window itself
 
         ### Enrolling Players Section ###
         
         add_player_layout = QVBoxLayout()
+        add_player_layout.addStretch(1)
 
         first_name_layout = QHBoxLayout()
         first_name_layout.setSpacing(1)
@@ -96,16 +103,49 @@ class EnrollPlayers(QWidget):
         button_layout.addWidget(self.clear_button)
 
         add_player_layout.addLayout(button_layout)
-
         add_player_layout.addStretch(1)
 
-        # FINISH MAKING THIS LOOK GOOD
+        begin_event_layout = QHBoxLayout()
+
+        begin_event_button = QPushButton('Begin Event', self)
+        begin_event_button.clicked.connect(self.beginEvent)
+
+        begin_event_layout.addStretch(1)
+
+        begin_event_layout.addWidget(begin_event_button)
+
+        add_player_layout.addLayout(begin_event_layout)
+
+        ### Table View For Enrolled Players Section ###
+
+        self.enrolled_table = QTableWidget()
+
+        # The following options all change how the user interacts
+        # with the table
+        
+        self.enrolled_table.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        self.enrolled_table.setSelectionBehavior(QAbstractItemView.SelectRows)
+        self.enrolled_table.verticalHeader().hide()
+        # These prevent the user from editing the table cells,
+        # Select the whole row when one is clicked
+        # and hide the headers that appear to the left of the rows.
+        
+        self.enrolled_table.setColumnCount(2)
+        self.enrolled_table.setHorizontalHeaderItem(0, QTableWidgetItem('Player'))
+        self.enrolled_table.setHorizontalHeaderItem(1, QTableWidgetItem('ID'))
+        self.enrolled_table.setColumnWidth(0, 235)
+        self.enrolled_table.horizontalHeader().setSectionResizeMode(
+            QHeaderView.ResizeMode(2))
+        # ResizeMode(2) is fixed size, meaning the user can't change the size
+        # of the table
+
+        window_layout.addWidget(self.enrolled_table)
 
         ### Misc Window Settings ###
 
         window_layout.addLayout(add_player_layout)
 
-        self.setGeometry(300, 300, 300, 300)
+        self.setGeometry(200, 200, 700, 500)
         self.setWindowTitle('Tournament Name')
 
         self.playerFirstName.setFocus(True)
@@ -123,6 +163,10 @@ class EnrollPlayers(QWidget):
         self.playerLastName.setText('')
 
         self.playerFirstName.setFocus(True)
+
+    def beginEvent(self):
+
+        pass
 
 if __name__ == '__main__':
 
